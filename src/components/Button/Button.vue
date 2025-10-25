be6acebfc8e9f95d860a3eb2cfdbf3a6aed93814
chenting <3213043406@qq.com>
2025-10-25 11:54:10 +0800

    项目初始化

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,44 @@
+<template>
+  <button
+    ref="_ref"
+    class="vk-button"
+    :class="{
+      [`vk-button--${type}`]: type,
+      [`vk-button--${size}`]: size,
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle,
+      'is-disabled': disabled,
+    }"
+    :disabled="disabled"
+    :autofocus="autofocus"
+    :type="nativeType"
+  >
+    <span>
+      <slot />
+    </span>
+  </button>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { ButtonProps } from './types'
+// import { buttonProps } from './types'
+defineOptions({
+  name: 'VkButton'
+})
+
+withDefaults(defineProps<ButtonProps>(), {
+  nativeType: 'button'
+})
+
+const _ref = ref<HTMLButtonElement>()
+
+defineExpose({
+  ref: _ref
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
