0c37b991193a13c602c8cc20a4ff6e3b2d9932b9
chenting <3213043406@qq.com>
2025-10-25 18:31:11 +0800

    Collapse-使用 Context 完成父子属性传递

--- src/components/Collapse/Collapse.vue
+++ src/components/Collapse/Collapse.vue
@@ -1,21 +1,29 @@
-<!-- <template>
-  <div class="vk-collapse">
-    <slot></slot>
-  </div>
-</template>
-<script setuo lang="ts">
-defineOptions({
-  name: "VkCollapse",
-});
-</script> -->
-
 <template>
   <div class="vk-collapse">
     <slot />
   </div>
 </template>
 <script setup lang="ts">
+import { ref, provide } from "vue";
+import type { NameType } from "./types";
+import { collapseContextKey } from "./types";
+
 defineOptions({
   name: "VkCollapse",
+});
+
+const activeNames = ref<NameType[]>([]);
+const handleItemClick = (item: NameType) => {
+  const index = activeNames.value.indexOf(item);
+  if (index > -1) {
+    activeNames.value.splice(index, 1);
+  } else {
+    activeNames.value.push(item);
+  }
+};
+
+provide(collapseContextKey, {
+  activeNames,
+  handleItemClick,
 });
 </script>
--- src/components/Collapse/CollapseItem.vue
+++ src/components/Collapse/CollapseItem.vue
@@ -5,19 +5,41 @@
       'is-disabled': disabled,
     }"
   >
-    <div class="vk-collapse-item__header" :id="`item-header-${name}`">
+    <div
+      class="vk-collapse-item__header"
+      :id="`item-header-${name}`"
+      @click="handleItemClick"
+    >
       <slot name="title">{{ title }}</slot>
     </div>
-    <div class="vk-collapse-item__content" :id="`item-content-${name}`">
+    <div
+      class="vk-collapse-item__content"
+      :id="`item-content-${name}`"
+      v-show="isActive"
+    >
       <slot />
     </div>
   </div>
 </template>
 <script setup lang="ts">
+import { inject, computed } from "vue";
 import type { CollapseItemProps } from "./types";
+import { collapseContextKey } from "./types";
+
 defineOptions({
   name: "VkCollapseItem",
 });
-defineProps<CollapseItemProps>();
+const props = defineProps<CollapseItemProps>();
+
+const collapseContext = inject(collapseContextKey);
+const isActive = computed(() =>
+  collapseContext?.activeNames.value.includes(props.name)
+);
+const handleItemClick = () => {
+  if (props.disabled) {
+    return;
+  }
+  collapseContext?.handleItemClick(props.name);
+};
 </script>
 <style></style>
