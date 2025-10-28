481926aec4885d4d823cee5ebf64ec846a369042
chenting <3213043406@qq.com>
2025-10-28 20:16:14 +0800

    createMessage-组件测试

--- src/components/Message/method.ts
+++ src/components/Message/method.ts
@@ -57,3 +57,9 @@ export const getLastBottomOffset = (id: string) => {
     return prev.vm.exposed!.bottomOffset.value;
   }
 };
+
+export const closeAll = () => {
+  instances.forEach((instance) => {
+    instance.destory();
+  });
+};
