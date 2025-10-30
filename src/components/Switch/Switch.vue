815ea8cf3f4c233fca183f9139f5be85937766a0
chenting <3213043406@qq.com>
2025-10-30 17:14:12 +0800

    Switch-基础编码

---
+++ src/components/Switch/Switch.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    class="vk-switch"
+    :class="{
+      [`vk-switch--${size}`]: size,
+      'is-disabled': disabled,
+      'is-checked': checked,
+    }"
+  >
+    <input
+      class="vk-switch__input"
+      type="checkbox"
+      role="switch"
+      :name="name"
+      :disabled="disabled"
+    />
+    <div class="vk-switch__core">
+      <div class="vk-switch__core-action"></div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { SwitchProps, SwitchEmits } from "./types";
+import { ref, computed } from "vue";
+
+defineOptions({
+  name: "VkSwitch",
+  inheritAttrs: false,
+});
+
+const props = defineProps<SwitchProps>();
+const emits = defineEmits<SwitchEmits>();
+
+const innerValue = ref(props.modelValue);
+
+const checked = computed(() => innerValue.value);
+</script>
