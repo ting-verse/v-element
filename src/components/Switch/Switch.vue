e0c5202554c3a2e91db50945a0f17f11dbacea2b
chenting <3213043406@qq.com>
2025-10-30 18:00:59 +0800

    Switch-多状态同步

--- src/components/Switch/Switch.vue
+++ src/components/Switch/Switch.vue
@@ -9,11 +9,13 @@
     @click="switchValue"
   >
     <input
-      class="vk-switch__input"
+      class="vk-swtich__input"
       type="checkbox"
       role="switch"
+      ref="input"
       :name="name"
       :disabled="disabled"
+      @keydown.enter="switchValue"
     />
     <div class="vk-switch__core">
       <div class="vk-switch__core-action"></div>
@@ -22,25 +24,36 @@
 </template>
 
 <script setup lang="ts">
+import { ref, computed, onMounted, watch } from "vue";
 import type { SwitchProps, SwitchEmits } from "./types";
-import { ref, computed } from "vue";
 
 defineOptions({
-  name: "VkSwitch",
+  name: "VkSwtich",
   inheritAttrs: false,
 });
-
 const props = defineProps<SwitchProps>();
 const emits = defineEmits<SwitchEmits>();
 
 const innerValue = ref(props.modelValue);
-
+const input = ref<HTMLInputElement>();
+// 现在是否被选中
 const checked = computed(() => innerValue.value);
-
 const switchValue = () => {
   if (props.disabled) return;
   innerValue.value = !checked.value;
   emits("update:modelValue", innerValue.value);
   emits("change", innerValue.value);
 };
+onMounted(() => {
+  input.value!.checked = checked.value;
+});
+watch(checked, (val) => {
+  input.value!.checked = val;
+});
+watch(
+  () => props.modelValue,
+  (newValue) => {
+    innerValue.value = newValue;
+  }
+);
 </script>
