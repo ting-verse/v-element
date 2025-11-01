e96b9741127625f2b36ddb2fc10ae55aae3fd47d
chenting <3213043406@qq.com>
2025-11-01 19:41:25 +0800

    Form-基础编码

---
+++ docs/demo/Form/Basic.vue
@@ -0,0 +1,30 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Form from "@/components/Form/Form.vue";
+import FormItem from "@/components/Form/FormItem.vue";
+import Input from "@/components/Input/Input.vue";
+import Button from "@/components/Button/Button.vue";
+</script>
+
+<template>
+  <div>
+    <Form>
+      <FormItem label="the email">
+        <Input />
+      </FormItem>
+      <FormItem label="the password">
+        <template #label="{ label }">
+          <Button>{{ label }}</Button>
+        </template>
+        <Input type="password" />
+      </FormItem>
+      <div>
+        <Button type="primary">Submit</Button>
+        <Button>Reset</Button>
+      </div>
+    </Form>
+  </div>
+</template>
+
+<style></style>
---
+++ src/components/Form/Form.vue
@@ -0,0 +1,11 @@
+<template>
+  <form class="vk-form">
+    <slot></slot>
+  </form>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: "VkForm",
+});
+</script>
